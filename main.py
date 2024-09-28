7dfa344ecb5b4866d9da78aeedd893eccee49657
taro <groveleffect@gmail.com>
2024-09-28 14:40:16 +0900

    ウインドウ表示処理の追加

---
+++ main.py
@@ -0,0 +1,20 @@
+# 標準ライブラリ
+import os
+import sys
+
+# 外部ライブラリ
+import PySide6
+from PySide6.QtWidgets import QApplication,QWidget
+
+# デフォルトのウインドウ
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        # 親クラスの初期化
+        super().__init__(parent)
+
+# メイン処理
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
