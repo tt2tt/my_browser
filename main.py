5d3e0ed7dba699723757e38f8fae9035dca4dceb
taro <groveleffect@gmail.com>
2024-09-28 19:46:15 +0900

    Webページの表示機能(URL参照)の追加

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import sys
 
 # 外部ライブラリ
 import PySide6
-from PySide6.QtWidgets import QApplication,QWidget
+from PySide6.QtWidgets import QApplication,QWidget,QVBoxLayout
+from PySide6.QtCore import QUrl
+from PySide6.QtWebEngineWidgets import QWebEngineView
 
 # デフォルトのウインドウ
 class MainWindow(QWidget):
@@ -12,6 +14,18 @@ class MainWindow(QWidget):
         # 親クラスの初期化
         super().__init__(parent)
         self.setWindowTitle("My Browser")
+
+        # Webページの読み込み
+        self.webview = QWebEngineView()
+        self.webview.load(QUrl("https://www.python.org/"))
+
+        # レイアウトの作成
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.webview)
+
+        # メインレイアウトの指定
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
 
 # メイン処理
 if __name__ == "__main__":
