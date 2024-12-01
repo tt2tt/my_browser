6f9f66bbbe9fc6f0d33bfddcd6d29f48a0eb4a75
taro <groveleffect@gmail.com>
2024-12-01 20:35:05 +0900

    タブ追加機能の追加

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QTabWidget
 import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QTabWidget, QToolBar, QPushButton
 
 from browser_tab import BrowserTab
 
@@ -14,9 +15,27 @@ class MainWindow(QMainWindow):
         self.tabs = QTabWidget()
         self.setCentralWidget(self.tabs)
 
-        # タブを追加
+        # ツールバーを作成
+        self.toolbar = QToolBar("メインツールバー")
+        self.addToolBar(self.toolbar)
+
+        # 初期タブ
         self.browser_tab = BrowserTab()
         self.tabs.addTab(self.browser_tab, "新しいタブ")
+
+        # タブ番号
+        self.tab_counter = 1
+
+        # タブ追加ボタン
+        add_tab_action = QPushButton("タブ追加")
+        add_tab_action.clicked.connect(self.add_new_tab)
+        self.toolbar.addWidget(add_tab_action)
+
+    def add_new_tab(self):
+        self.tab_counter += 1
+        self.browser_tab = BrowserTab()
+        self.tabs.addTab(self.browser_tab, "新しいタブ")
+
 
 if __name__ == "__main__":
     # メインウインドウの呼び出し
