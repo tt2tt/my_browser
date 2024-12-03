5f9b7aac4d793d4d31d6d651c78c59603a263798
taro <groveleffect@gmail.com>
2024-12-03 21:37:23 +0900

    URLバーの追加

--- browser_tab.py
+++ browser_tab.py
@@ -1,11 +1,18 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout
 from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtCore import QUrl
 
 class BrowserTab(QWidget):
     def __init__(self):
         super().__init__()
         self.layout = QVBoxLayout()
-        self.browser = QWebEngineView()
-        self.browser.setUrl("https://www.google.com")
-        self.layout.addWidget(self.browser)
+        self.web_view = QWebEngineView()
+        self.web_view.setUrl("https://www.google.com")
+        self.layout.addWidget(self.web_view)
         self.setLayout(self.layout)
+
+    # URLの検索
+    def load_url(self,url):
+        if not url.startswith("http://") and not url.startswith("https://"):
+            url = "http://" + url
+        self.web_view.setUrl(QUrl(url))
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QTabWidget, QToolBar, QPushButton
-
+from PySide6.QtWidgets import QApplication, QMainWindow, QTabWidget, QToolBar, QPushButton, QLineEdit
 from browser_tab import BrowserTab
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, browser_tab):
         super().__init__()
         # メインウインドウ設定
         self.setWindowTitle("マイブラウザ")
@@ -22,17 +21,20 @@ class MainWindow(QMainWindow):
         self.toolbar = QToolBar("メインツールバー")
         self.addToolBar(self.toolbar)
 
-        # 初期タブ
-        self.browser_tab = BrowserTab()
-        self.tabs.addTab(self.browser_tab, "新しいタブ")
-
-        # タブ番号
-        self.tab_counter = 1
+        # URLバー
+        self.url_bar = QLineEdit()
+        self.url_bar.setPlaceholderText("URLを入力して下さい")
+        self.url_bar.returnPressed.connect(lambda: browser_tab.load_url(self.url_bar.text()))
+        self.toolbar.addWidget(self.url_bar)
 
         # タブ追加ボタン
+        self.tab_counter = 1
         add_tab_action = QPushButton("タブ追加")
         add_tab_action.clicked.connect(self.add_new_tab)
         self.toolbar.addWidget(add_tab_action)
+
+        # 初期タブ
+        self.tabs.addTab(browser_tab, "新しいタブ")
 
     # タブの追加
     def add_new_tab(self):
@@ -49,6 +51,7 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     # メインウインドウの呼び出し
     app = QApplication(sys.argv)
-    main_window = MainWindow()
+    browser_tab = BrowserTab()
+    main_window = MainWindow(browser_tab)
     main_window.show()
     sys.exit(app.exec())
