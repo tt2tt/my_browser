3ed714d8da4f8a9e32438bdbf3768cea56101362
taro <groveleffect@gmail.com>
2024-12-07 09:57:12 +0900

    WebのPDF表示機能の追加

--- browser_tab.py
+++ browser_tab.py
@@ -1,12 +1,15 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl
+from PySide6.QtWebEngineCore import QWebEngineSettings
 
 class BrowserTab(QWidget):
     def __init__(self):
         super().__init__()
         self.layout = QVBoxLayout()
         self.web_view = QWebEngineView()
+        self.web_view.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+        self.web_view.settings().setAttribute(QWebEngineSettings.PdfViewerEnabled, True)
         self.web_view.setUrl("https://www.google.com")
         self.layout.addWidget(self.web_view)
         self.setLayout(self.layout)
